0f445d46bdbdd36a8fd13bb7d5447a117e91808b
Misha <m.skvortsovvv@gmail.com>
2022-01-09 18:54:34 +0300

    2

--- src/main/java/com/example/prodj/controller/AboutController.java
+++ src/main/java/com/example/prodj/controller/AboutController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AboutController {
     @GetMapping("/about")
     public String blogMain(Model module){
-        module.addAttribute("title", "Главная страница");
+        module.addAttribute("title", "Наша страница");
         return "about";
     }
 }
--- src/main/java/com/example/prodj/controller/BlogController.java
+++ src/main/java/com/example/prodj/controller/BlogController.java
@@ -1,12 +1,14 @@
 package com.example.prodj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
     @GetMapping("/blog")
-    public String blogMain(Module module){
+    public String blogMain(Model module){
+        module.addAttribute("title", "Наша страница");
         return "blog-main";
     }
 }
